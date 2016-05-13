78eb96c55868c6b296c71b6e0f0528484b8b0f53
Merlin Göttlinger <megoettlinger@gmail.com>
2016-05-13 11:34:43 +0200

    Defcustom instead of const and added a few rules

--- scala-mode2-prettify-symbols.el
+++ scala-mode2-prettify-symbols.el
@@ -1,56 +1,74 @@
-;;; scala-mode-prettify.el - Extension of scala-mode for prettifying scala symbols/code
 ;;; scala-mode2-prettify-symbols.el - Extension of scala-mode for prettifying scala symbols/code
 ;;; Copyright (c) 2016 Merlin Göttlinger
 ;;; For information on the License, see the LICENSE file
 ;; -*- coding: UTF-8 -*-
 
-(defconst scala-mode-pretty-bool-alist '(
-					 ("<=" . ?≤)
-					 (">=" . ?≥)
-					 ("==" . ?≡)
-					 ("!" . ?¬)
-					 ("!=" . ?≢)
-					 ("&&" . ?∧)
-					 ("||" . ?∨)
-					 ("true" . ?⊤)
-					 ("false" . ?⊥)
-					 ("Boolean" . ?𝔹)))
+(defcustom scala-mode-pretty-bool-alist '(
+					  ("<=" . ?≤)
+					  (">=" . ?≥)
+					  ("==" . ?≡)
+					  ("===" . ?≣)
+					  ("!" . ?¬)
+					  ("!=" . ?≢)
+					  ("&&" . ?∧)
+					  ("||" . ?∨)
+					  ("true" . ?⊤)
+					  ("false" . ?⊥)
+					  ("Boolean" . ?𝔹))
+  "Prettify rules for boolean related operations."
+  :type 'alist)
 
-(defconst scala-mode-pretty-collection-alist '(
-					       ("empty" . ?∅)
-					       ("sum" . ?∑)
-					       ("contains" . ?∍)
-					       ("++" . ?⧺)
-					       ("::" . ?⸬)
-					       ("--" . ?╌)))
+(defcustom scala-mode-pretty-collection-alist '(
+						("empty" . ?∅)
+						("sum" . ?∑)
+						("product" . ?∏)
+						("contains" . ?∍)
+						("forall" . ?∀)
+						("any" . ?∃)
+						("intersect" . ?∩)
+						("union" . ?∪)
+						("diff" . ?≏)
+						("subsetOf" . ?⊆)
+						("++" . ?⧺)
+						("::" . ?⸬)
+						("--" . ?╌))
+  "Prettify rules for collections related operations."
+  :type 'alist)
 
-(defconst scala-mode-pretty-arrows-alist'(
-					  ("->" . ?→)
-					  ("<-" . ?←)
-					  ("=>" . ?⇒)
+(defcustom scala-mode-pretty-arrows-alist'(
+					   ("->" . ?→)
+					   ("<-" . ?←)
+					   ("=>" . ?⇒)
 					;("<=" . ?⇐)
-					  ("<=>" . ?⇔)
-					  ("-->" . ?⟶)
-					  ("<->" . ?↔)
-					  ("<--" . ?⟵)
-					  ("<-->" . ?⟷)
-					  ("==>" . ?⟹)
-					  ("<==" . ?⟸)
-					  ("<==>" . ?⟺)
-					  ("~>" . ?⇝)
-					  ("<~" . ?⇜)))
+					   ("<=>" . ?⇔)
+					   ("-->" . ?⟶)
+					   ("<->" . ?↔)
+					   ("<--" . ?⟵)
+					   ("<-->" . ?⟷)
+					   ("==>" . ?⟹)
+					   ("<==" . ?⟸)
+					   ("<==>" . ?⟺)
+					   ("~>" . ?⇝)
+					   ("<~" . ?⇜))
+  "Prettify rules for arrow related code pieces."
+  :type 'alist)
 
-(defconst scala-mode-pretty-misc-alist '(
-					 ("null" . ?∅)
-					 ("Nothing" . ?∅)
-					 ("Unit" . ?∅)
-					 ("Int" . ?ℤ)
-					 (":=" . ?≔)))
+(defcustom scala-mode-pretty-misc-alist '(
+					;("null" . ?∅)
+					;("Nothing" . ?∅)
+					  ("Unit" . ?∅)
+					  ("Int" . ?ℤ)
+					  ("assert" . ?⊦)
+					  (":=" . ?≔))
+  "Prettify rules for other mixed code pieces."
+  :type 'alist)
 
-(defconst scala-mode-pretty-all-alist (append
-				       scala-mode-pretty-bool-alist
-				       scala-mode-pretty-collection-alist
-				       scala-mode-pretty-arrows-alist
-				       scala-mode-pretty-misc-alist))
+(defcustom scala--prettify-symbols-alist (append
+					  scala-mode-pretty-bool-alist
+					  scala-mode-pretty-collection-alist
+					  scala-mode-pretty-arrows-alist
+					  scala-mode-pretty-misc-alist)
+  "All prettify rules to be applied in scala code."
+  :type 'alist)
 
 (provide 'scala-mode2-prettify-symbols)
